d24046f94c4133a97cfec956facc4fa82745e59f
willbowden <willbowden03@yahoo.co.uk>
2021-10-21 18:42:29 +0100

    Struggling with the algorithm

--- app/trainingsets.py
+++ app/trainingsets.py
@@ -8,17 +8,72 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 def score_dataset(ds: pd.DataFrame, timeframe: str) -> None:
-    results = {'dates': [], 'scores': []}
+    results = {'markers': [], 'dataset': ds}
     pointsInDay = int(constants.TIMEFRAME_MILLISECONDS['1d'] / constants.TIMEFRAME_MILLISECONDS[timeframe])
-    lastIndex = len(ds.index) - pointsInDay
-    for index, row in ds.iterrows():
-        offset = 0
-        if index <= lastIndex:
-            candleCount = 1
-            currentPrice = row['open']
-            for i in range(pointsInDay):
+    lastIndex = len(ds.index) - pointsInDay - 1
+    index = 0
+    while index <= lastIndex:
+        queue = []
+        startPrice = ds.iloc[[index]]['open'].iat[0]
+        startDate = ds.iloc[[index]]['date'].iat[0]
+        highest = startPrice
+        lowest = 1000000
+        markerFound = False
+        shift = 1
+        while not markerFound:
+            if index + shift + 1 <= lastIndex:
+                currentPrice = ds.iloc[[index + shift]]['close'].iat[0]
+                currentDate = ds.iloc[[index + shift]]['date'].iat[0]
+                if currentPrice > highest:
+                    highest = currentPrice
+                elif currentPrice < lowest:
+                    lowest = currentPrice
                 
-                pass
+                if currentPrice == highest and highest != startPrice:
+                    if highest >= 1.007 * startPrice:
+                        if ds.iloc[[index + shift + 1]]['close'].iat[0] <= 0.993 * currentPrice:
+                            results['markers'].append({'date': startDate, 'price': startPrice, 'score': 1})
+                            results['markers'].append({'date': currentDate, 'price': currentPrice, 'score': -1})
+                            index += shift
+                            markerFound = True
+                if currentPrice == lowest and lowest != 1000000:
+                    if lowest <= 0.993 * startPrice:
+                        results['markers'].append({'date': startDate, 'price': startPrice, 'score': -1})
+                        index += shift
+                        markerFound = True
+
+                shift += 1
+            else:
+                markerFound = True
+        
+        index += 1
+
+    plot_results(results)
+
+
+
+def plot_results(results):
+    buyMarkers = [x for x in results['markers'] if x['score'] == 1]
+    sellMarkers = [x for x in results['markers'] if x['score'] == -1]
+    fig = make_subplots(rows=1, cols=1)
+    fig.add_trace(go.Scatter(x=results['dataset']['date'], y=results['dataset']['close'], name="Price", line_color="black"), row=1, col=1)
+    fig.add_trace(go.Scatter(
+            x=[x['date'] for x in buyMarkers],
+            y=[x['price'] for x in buyMarkers],
+            mode='markers',
+            name='Scores',
+            text = "BUY",
+            line_color='green'), row=1, col=1)
+
+    fig.add_trace(go.Scatter(
+            x=[x['date'] for x in sellMarkers],
+            y=[x['price'] for x in sellMarkers],
+            mode='markers',
+            name='Scores',
+            text = "SELL",
+            line_color='red'), row=1, col=1)
+
+    fig.show()
 
 
 def create_training_set():
@@ -35,4 +90,5 @@ def gather_datasets(symbols: list, timeframe: str, startDate: int) -> pd.DataFra
     return total
 
 if __name__ == '__main__':
-    ds = gather_datasets(['ETH/USDT'], '1h', 1618873200000)
+    ds = datasets.load_dataset('ETH/USDT', '1h', 1602975600000, {'requiredIndicators': ['rsi']})
+    score_dataset(ds, '1h')
