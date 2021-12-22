8205798bfbc88136fb5b21882292e6be679ca200
willbowden <willbowden03@yahoo.co.uk>
2021-12-22 18:50:28 +0000

    All exchange data functions finished.  Pagination implemented & LOADS of writeup.
    Worked for 9 hours straight.

--- server/exchange.py
+++ server/exchange.py
@@ -5,7 +5,7 @@
 import ccxt
 import datetime
 import pandas as pd
-from SmartTrade.server import conversions
+from SmartTrade.server import conversions, constants
 
 class Exchange: # Class to represent an exchange object. This is necessary as each user has their own unique exchange object linked to their API key
     def __init__(self, id, apiKey, secret):
@@ -19,52 +19,188 @@ class Exchange: # Class to represent an exchange object. This is necessary as ea
         })
         self.markets = self.exchange.load_markets()
 
-    def fetch_balance(self): # Get a user's cryptocurrency balances
+    # General purpose function to download data using pagination
+    def __use_pagination(self, func, since, timeArgName, kwargs={}) -> list:
+        end = self.exchange.milliseconds() - 1000 # Use the current time as the end date.
+        whole = []
+        sameSinceCount = 0
+        while since < end and sameSinceCount < 3: 
+            # If the latest data point is earlier than the end date,
+            #   we'd get stuck in a loop. So keep count of repeating data.
+
+            if 'params' in kwargs: # Handling custom API requests such as fetching fiat trades.
+                kwargs['params']['beginTime'] = since
+                new  = func(kwargs['params'])
+            else:
+                kwargs['since'] = since
+                new = func(**kwargs) # Request data from exchange using provided function.
+
+            if new == []:
+                return whole
+            latestDate = int(new[-1][timeArgName])
+            if latestDate == since: # If we see a repeated date, don't append repeated data.
+                sameSinceCount += 1
+            else:
+                whole += new
+                since = latestDate
+
+        return whole
+
+    def __reverse_pagination(self, func, start, timeArgName, endArgName, params={}, kwargs={}) -> list:
+        whole = []
+        sameSinceCount = 0
+        while sameSinceCount < 3: 
+            params[endArgName] = start
+            new = func(params=params, **kwargs)
+
+            if new == []:
+                return whole
+            earliestDate = int(new[0][timeArgName])
+            if earliestDate == start: # If we see a repeated date, don't append repeated data.
+                sameSinceCount += 1
+            else:
+                whole += new
+                start = earliestDate
+
+        return whole
+
+    def __custom_api_caller(self, func):
+        def temp(params):
+            try:
+                result = func(params)['data']
+            except Exception as e:
+                print(e)
+                result = []
+
+            return result
+        
+        return temp
+        
+    # Get a user's cryptocurrency balances
+    def fetch_balances(self):
         if self.exchange.has['fetchBalance']:
             return self.exchange.fetchBalance()
         else:
             raise Exception('Exchange does not support fetching balance.')
 
-    def fetch_ticker(self, symbol): # Get the most recent ~50 price points for a given symbol
+    # Get the most recent ~50 price points for a given symbol
+    def fetch_ticker(self, symbol): 
         if self.exchange.has['fetchTicker']:
             self.ticker=self.exchange.fetch_trades(symbol)
             return self.ticker
         else:
             raise Exception("Exchange does not support fetching tickers.")
 
-    def fetch_ohlcv(self, symbol, timeframe, since = None): # Get open, high, low, close, volume data for a given symbol and from a given date. Can only return ~500 datapoints at once
+    # Get open, high, low, close, volume data for a given symbol and from a given date. Can only return ~500 datapoints at once
+    def fetch_ohlcv(self, symbol, timeframe, since=1230768000000, limit=None) -> pd.DataFrame: 
         if isinstance(since, str):
             since = conversions.date_to_unix(since)
-        if self.exchange.has['fetchOHLCV']:
-            return self.exchange.fetch_ohlcv(symbol, timeframe, since)
+
+        if self.exchange.has['fetchOHLCV']: # Use pagination to fetch historical OHLCV data. Default start date is as early as possible.
+            whole = pd.DataFrame(columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+            sameSinceCount = 0
+            end = self.exchange.milliseconds() - 1000
+            # Estimate the number of datapoints to download.
+            toFetch = int(round(((end-since)/constants.TIMEFRAME_MILLISECONDS[timeframe]), 0))
+            print(f'Downloading historical data for {symbol}. Approximate number of datapoints to fetch: {toFetch}')
+            while since < end and sameSinceCount < 3:
+                toAppend = pd.DataFrame(self.exchange.fetch_ohlcv(symbol, timeframe, since, limit), columns=whole.columns) # Fetch data
+                if since == toAppend['timestamp'].iat[-1]:
+                    sameSinceCount += 1
+                else:
+                    since = toAppend['timestamp'].iat[-1]
+                    whole = whole.append(toAppend, ignore_index=True) # Append
+                    progress = ((len(whole) / toFetch) * 100) # Calculate progress
+                    print(f'{conversions.unix_to_date(since)} | {str(round(progress, 1))}% | {len(whole)} points downloaded.')
+
+        
+            whole.reset_index(inplace=True, drop=True) # Reset and remove the numerical index
+            return whole
         else:
             raise Exception("Exchange does not support fetching OHLCV.")
 
-    def fetch_trades(self, symbol=None, since=None, limit=None) -> list: # Fetch all of a user's trades since a given start date.
+    # Fetches the price of a given asset at any single point in time.
+    def fetch_price_at_time(self, symbol: str, date: int) -> float: 
+        data = self.exchange.fetch_ohlcv(symbol, '1m', date, limit=1)
+        return data[0][4] # Fetch the close price from the returned list
+
+    # Just return most recent close price from ticker
+    def fetch_current_price(self, symbol: str) -> float: 
+        return self.fetch_ticker(symbol)[-1]['price']
+
+    # Collect fiat trades using pagination. Default start date is as early as possible.
+    def fetch_fiat_trades(self, since=1230768000000, limit=None) -> pd.DataFrame: 
+        fiatBuys = self.__use_pagination(self.__custom_api_caller(self.exchange.sapi_get_fiat_payments),
+            since, 'updateTime', {'params': {'transactionType': "0", 'beginTime': since}, 'rows': limit})
+        fiatSells = self.__use_pagination(self.__custom_api_caller(self.exchange.sapi_get_fiat_payments),
+            since, 'updateTime', {'params': {'transactionType': "1", 'beginTime': since}, 'rows': limit})
+        
+        fiatTrades = fiatBuys + fiatSells # Combine them
+        fiatTrades = pd.DataFrame(fiatTrades)
+        
+        fiatTrades = fiatTrades[fiatTrades['status'] == 'Completed'] # Only consider successful orders
+        # Combine fiat and crypto currency symbols to make a pair e.g "BTC/GBP"
+        symbolPairs = [f"{row['cryptoCurrency']}/{row['fiatCurrency']}" for index, row in fiatTrades.iterrows()]
+        fiatTrades['symbol'] = symbolPairs # Add the pairs as a column
+        # Only take the columns we're interested in. Drop the rest
+        fiatTrades = fiatTrades.drop(columns=[col for col in fiatTrades if col not in ['symbol', 'updateTime', 'sourceAmount', 'obtainAmount', 'price', 'totalFee']])
+        # Rename the columns to conform with the rest of the data.
+        fiatTrades = fiatTrades.rename(columns={'updateTime': 'timestamp', 'sourceAmount': 'cost', 'obtainAmount': 'amount', 'totalFee': 'fee'})
+        fiatTrades = fiatTrades.sort_values(by=['timestamp']) # Sort into chronological order.
+        fiatTrades.reset_index(inplace=True, drop=True)
+
+        return fiatTrades
+
+    # Use pagination to fetch all crypto trades for a symbol since a given start date. Default is as early as possible.
+    def fetch_crypto_trades(self, symbol=None, since=1230768000000, limit=None) -> list:
         if self.exchange.has['fetchMyTrades']:
-            return self.exchange.fetch_my_trades(symbol=symbol, since=since, limit=limit)
+            return self.__use_pagination(self.exchange.fetch_my_trades, since, 'timestamp', {'symbol': symbol, 'limit': limit})
         else:
-            raise Exception("Exchange does not support fetching user trades.")
-
-    def fetch_orders(self, symbol=None, since=None, limit=None) -> list: # Fetch all of a user's trades since a given start date.
+            raise Exception("Exchange does not support fetching user crypto trades.")
+        
+    # Use pagination to fetch all of a user's orders for a symbol since a given start date. Default is earliest possible date.
+    def fetch_orders(self, symbol=None, since=1230768000000, limit=None) -> list: 
         if self.exchange.has['fetchOrders']:
-            return self.exchange.fetch_orders(symbol=symbol, since=since, limit=limit)
+            return self.__use_pagination(self.exchange.fetch_orders, since, 'timestamp', {'symbol': symbol, 'limit': limit})
         else:
             raise Exception("Exchange does not support fetching user trades.")
 
-
-    def fetch_deposits(self, since=None, limit=None) -> list: # Fetch all of a user's deposits since a given start date.
+    # Fetch all of a user's deposits prior to a given start date. Default is latest possible date.
+    def fetch_crypto_deposits(self, latestDate=None) -> list: 
         if self.exchange.has['fetchDeposits']:
-            return self.exchange.fetch_deposits(since=since, limit=limit)
+            if latestDate is None:
+                latestDate = self.exchange.milliseconds()
+            return self.__reverse_pagination(self.exchange.fetch_deposits, latestDate, 'timestamp', 'endTime')
         else:
             raise Exception("Exchange does not support fetching deposits.")
 
-    def fetch_withdrawals(self, since=None, limit=None) -> list: # Fetch all of a user's withdrawals since a given start date.
+    # Fetch all of a user's withdrawals prior to a given start date. Default is latest possible date.
+    def fetch_crypto_withdrawals(self, latestDate=None) -> list: 
         if self.exchange.has['fetchWithdrawals']:
-            return self.exchange.fetch_withdrawals(since=since, limit=limit)
+            if latestDate is None:
+                latestDate = self.exchange.milliseconds()
+            return self.__reverse_pagination(self.exchange.fetch_withdrawals, latestDate, 'timestamp', 'endTime')
         else:
             raise Exception("Exchange does not support fetching withdrawals.")
 
-    def place_order(self, symbol, side, type, price, quantity): # Places an order on the exchange on behalf of a user
+    # Fetch all of a user's fiat deposits since a given start date. Default is earliest possible date.
+    def fetch_fiat_deposits(self, latestDate=None) -> list:
+        if latestDate is None:
+                latestDate = self.exchange.milliseconds()
+        fiatDeposits = self.__reverse_pagination(self.__custom_api_caller(self.exchange.sapi_get_fiat_orders),
+            latestDate, 'updateTime', 'endTime', {'transactionType': "0"})
+        return fiatDeposits
+
+    # Fetch all of a user's fiat withdrawals since a given start date. Default is earliest possible date.
+    def fetch_fiat_withdrawals(self, latestDate=None) -> list:
+        if latestDate is None:
+                latestDate = self.exchange.milliseconds()
+        fiatWithdrawals = self.__reverse_pagination(self.__custom_api_caller(self.exchange.sapi_get_fiat_orders),
+            latestDate, 'updateTime', 'endTime', {'transactionType': "1"})
+        return fiatWithdrawals
+
+    # Places an order on the exchange on behalf of a user
+    def place_order(self, symbol, side, type, price, quantity): 
         return
+
 
--- testing_getting_transactions.py
+++ testing_getting_transactions.py
@@ -4,8 +4,6 @@ import time
 from SmartTrade.server import dbmanager, exchange_data
 import pandas as pd
 
-u = User(dbmanager.get_row_by_column('tblUsers', 'userID', 2094))
-
 def get_traded_pairs(user):
     tradedPairs = []
     toCheck = list(user.exchange.markets.keys()) # Get all available trading pairs on the exchange.
@@ -26,28 +24,42 @@ def get_traded_pairs(user):
 
     return tradedPairs
 
+def __get_by_pagination(function, params, since, end):
+    total = pd.DataFrame()
+    while since < end:
+        total = total.append(function(*params))
+        since = total['timestamp'][-1]
+
 def __get_deposits_and_withdrawals(user): # Get all deposits and withdrawals made by a user.
     df = pd.DataFrame()
-    df = df.append(user.exchange.fetch_deposits()) # Collect data from exchange
-    df = df.append(user.exchange.fetch_withdrawals())
-    df = df.drop(columns=[col for col in df if col not in ['info', 'timestamp', 'amount', 'type']]) # Filter out only the needed info
-    df['coin'] = [row['info']['coin'] for index, row in df.iterrows()] # Extract the 'coin' info from the 'info' dict.
-    df = df.drop(columns=['info']) # Get rid of the info dict, as we don't need anything more from it.
-    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms') # Make sure all timestamps are in the right format, for sorting.
-    df.reset_index(inplace=True, drop=True) # Reset the index
+    df = df.append(user.exchange.exchange.fetch_deposits()) # Collect data from exchange
+    # df = df.append(user.exchange.fetch_withdrawals())
+    # df = df.drop(columns=[col for col in df if col not in ['info', 'timestamp', 'amount', 'type']]) # Filter out only the needed info
+    # df['coin'] = [row['info']['coin'] for index, row in df.iterrows()] # Extract the 'coin' info from the 'info' dict.
+    # df = df.drop(columns=['info']) # Get rid of the info dict, as we don't need anything more from it.
+    # df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms') # Make sure all timestamps are in the right format, for sorting.
+    # df.reset_index(inplace=True, drop=True) # Reset the index
 
     return df
 
-def track_transactions(user, tradedPairs): # Track a user's trading history and use it to populate the database.
+def track_transactions(user, tradedPairs, trades=None): # Track a user's trading history and use it to populate the database.
     # 'txns' is short for 'transactions'. Just quicker to type.
     balances = {}
-    # trades = pd.DataFrame()
-    trades = pd.read_json('trades.json')
-    # for pair in tradedPairs:
-        # print(f"Getting {pair}")
-        # balances[pair] = pd.DataFrame(columns=['timestamp', 'type', 'price', 'amount'])
-        # pairTrades = user.exchange.fetch_trades(pair)
-        # trades = trades.append(pd.DataFrame(pairTrades))
+    downloadTrades = False
+    if trades is None: # If not provided a user's trades, download a new set from exchange.
+        trades = pd.DataFrame()
+        downloadTrades = True
+
+    for pair in tradedPairs:
+        coins = pair.split("/") # Separate trading pairs into base and quote currency
+        for coin in coins:
+            if coin not in balances: # Add records for the balance of each coin.
+                balances[coin] = pd.DataFrame(columns=['timestamp', 'type', 'price', 'amount'])
+
+        if downloadTrades: # Download trades from exchange if necessary
+            print(f"Getting {pair}")
+            pairTrades = user.exchange.fetch_trades(pair)
+            trades = trades.append(pd.DataFrame(pairTrades))
 
     trades = trades.drop(columns=[col for col in trades if col not in ['timestamp', 'symbol', 'side', 'price', 'amount', 'cost', 'fee']]) # Extract needed info
     trades = trades.rename(columns={"side": "type"}) # Rename column to conform with deposits & withdrawals
@@ -58,18 +70,28 @@ def track_transactions(user, tradedPairs): # Track a user's trading history and 
 
     print(txns)
 
-    # for index, txn in txns.iterrows():
-    #     if trade['info']['isBuyer']:
-
-
-
-with open('tradedPairs.json', 'r') as infile:
-    tradedPairs = json.load(infile)
-
-track_transactions(u, tradedPairs)
-
-
-
-
-
+    for index, txn in txns.iterrows():
+        if type(txn['symbol']) == str: # If we're working with a trading pair..
+            base, quote = txn['symbol'].split("/") # .. separate into base and quote currencies.
+        if txn['type'] == 'buy':
+            pass
             
+
+
+
+# with open('tradedPairs.json', 'r') as infile:
+#     tradedPairs = json.load(infile)
+
+# trades = pd.read_json('trades.json')
+# track_transactions(u, tradedPairs, trades)
+
+
+u = User(dbmanager.get_row_by_column('tblUsers', 'userID', 2094))
+ex = u.exchange
+with open('test.json', 'w') as outfile:
+    result = ex.fetch_fiat_withdrawals()
+    json.dump(result, outfile)
+
+# print(ex.exchange.fetch_deposits())
+
+# print(ex.exchange.sapi_get_fiat_orders(params={'transactionType': 1}))
