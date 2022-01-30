b26c93487ef52d8dcefe86627c3038d828e95bd7
willbowden <willbowden03@yahoo.co.uk>
2022-01-30 22:24:13 +0000

    Table of account holdings

---
+++ app/src/components/holdingsTable.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import protectedFetch from "../auth/protectedFetch.js";
+import { Button, CircularProgress, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+
+export default function HoldingsTable() {
+    const [holdings, setHoldings] = useState(null);
+    const [refresh, setRefresh] = useState(false);
+
+    useEffect(() => {
+        protectedFetch('/api/get_user_holdings').then(result => {
+        let asArray = []
+        for (const [key, value] of Object.entries(result)) {
+            if (key != 'totalValue') {
+            asArray.push({asset: key, quantity: value.balance, value: value.value})
+            }
+        }
+        asArray.sort(function(first, second) {
+            return second.value - first.value;
+        });
+        setHoldings(asArray);
+        })
+    }, [refresh])
+
+    function onRefreshClick(e) {
+        e.preventDefault();
+        refresh ? setRefresh(false) : setRefresh(true);
+        setHoldings(null);
+    }
+
+    return (
+    <TableContainer component={Paper} sx={{tableLayout: 'auto'}}>
+        <Table>
+        <TableHead>
+        <TableRow>
+            <TableCell>Asset</TableCell>
+            <TableCell>Quantity</TableCell>
+            <TableCell>Value</TableCell>
+        </TableRow>
+        </TableHead>
+        <TableBody>
+        { holdings ? holdings.map((row) => (
+            <TableRow key={row.asset}>
+            <TableCell>{row.asset}</TableCell>
+            <TableCell>{row.quantity}</TableCell>
+            <TableCell>{row.value}</TableCell>
+            </TableRow> 
+        )) : <CircularProgress />}
+        </TableBody>
+        </Table>
+        <Button variant="contained" onClick={onRefreshClick}>Refresh</Button>
+  </TableContainer>)
+}
--- app/src/routes/Dashboard.jsx
+++ app/src/routes/Dashboard.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import CenteredPageContainer from '../components/centeredPageContainer.jsx';
-import { Grid, Box, Typography, Button, Item } from '@mui/material';
+import { Grid } from '@mui/material';
+import HoldingsTable from "../components/holdingsTable.jsx";
 
 
 function Dashboard() {
+  
   return ( 
     <CenteredPageContainer>
-      <Grid item xs={6} >
-        Ayo
-      </Grid>
-      <Grid item xs={6}>
-        Ayo
+      <Grid item xs={4} >
+        <HoldingsTable />
       </Grid>
     </CenteredPageContainer>
   );
